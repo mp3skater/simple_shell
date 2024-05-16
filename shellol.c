bd034027cc0663c88f05a0ec308ca53508ee5cea
mp3skater <mp3skaterr@gmail.com>
2024-05-16 11:58:44 +0200

    shellol added

---
+++ shellol.c
@@ -0,0 +1,62 @@
+/*
+ * Module: shellol.c
+ * Author: mp3skater
+ * License: MIT License
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+  char prog[15];
+
+  // Program runs until you type "q"
+  while(strcmp(prog,"q") != 0)
+  {
+    // Get working directory and user-id
+    char cwd[30];
+    getcwd(cwd, 29);
+    int uid = getuid();
+    printf("%i:%s$ ", uid, cwd);
+
+    // Get name of call
+    char prog[15];
+    scanf("%s", prog);
+
+
+
+    // Exit
+    if(strcmp(prog,"q") != 0) {}
+
+    // Print Working Directory
+    else if(strcmp(prog,"pwd") != 0) {
+      printf("%s\n", cwd);
+    }
+
+    // Print User ID
+    else if(strcmp(prog,"puid") != 0) {
+      printf("%i\n", uid);
+    }
+
+    // Program call
+    else {
+      int parent = fork();
+
+      // Parent
+      if(parent) {
+        wait();
+      }
+
+      // Child
+      else{
+        execlp(prog, prog);
+
+        printf("Can't find program called: \"%s\".\n", prog);
+
+        return 1;
+      }
+    }
+  }
+}
